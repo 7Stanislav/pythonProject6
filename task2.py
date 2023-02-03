2d2aa130a81826c81c3cc92ec1eebfb464981c0a
7Stanislav <112968442+7Stanislav@users.noreply.github.com>
2023-02-03 17:53:04 +0200

    Completed task 2

--- task2.py
+++ task2.py
@@ -6,3 +6,9 @@
 Результат: [12, 44, 4, 10, 78, 123].
 Реализуйте с list comprehension
 """
+
+from random import randint
+numbers = [randint(0, 301) for i in range(13)]
+print(f"Пример исходного списка: {numbers}")
+new_list = [el for num, el in enumerate(numbers) if numbers[num - 1] < numbers[num]]
+print(new_list)
